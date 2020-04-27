b8b7a6a93136f7a96fbadf63be4645b9489fe147
Frank Garcia <garcia1901l@gmail.com>
2020-04-26 22:31:47 -0500

    Buscador de peliculas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    movie: {},
+    isFetching: false 
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault()
+
+    this.setState({ isFetching: true })
+
+    const title = event.target[0].value
+    const url = 'https://api.jikan.moe/v3/search/anime'
+
+    axios.get(url, {
+      params: {
+        q: title,
+        limit : 1
+      }
+    })
+      .then(res => this.setState({
+        movie: res.data.results[0],
+        isFetching: false
+    }))
+  }
+
+  render () {
+    const { movie, isFetching } = this.state
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder='Nombre de Pelicula'
+          />
+          <button>
+            Buscar
+          </button>
+        </form>
+        {isFetching && (
+          <h2>Cargando...</h2>
+        )}
+        { movie.title && !isFetching && (
+          <div>
+            <h1>{ movie.title }</h1>
+            <p>
+              { movie.synopsis }
+            </p>
+            <img
+              src={ movie.image_url }
+              alt='Poster'
+              style={{
+                width: '150px'
+              }}  
+            />
+          </div>
+        ) }
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
